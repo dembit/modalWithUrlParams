8c6bd8f3b9398c406ebf43fa6a61ac65c3d7e119
Dmitry <dembit1991@gmail.com>
2024-10-26 23:14:26 +0700

    0.0.1

--- Dialog.tsx
+++ Dialog.tsx
@@ -17,18 +17,19 @@ export default function Dialog({ children, modalType }: Props) {
     dialogRef.current?.[isOpen ? "showModal" : "close"]();
   }, [modalParameter, modalType]);
 
-  return (
-    <dialog
-      className="fixed  backdrop:bg-gray-500/50 backdrop:backdrop-blur-sm  "
-      ref={dialogRef}
-    >
-      <button
-        className="absolute right-0 top-0 bg-red-400 rounded-lg cursor-pointer"
-        onClick={onCloseModal}
-      >
-        X
-      </button>
-      {children}
+  const dialog: JSX.Element | null = modalParameter ? (
+    <dialog ref={dialogRef}>
+      <div className="relative">
+        <div
+          className="absolute right-0 top-0 bg-red-400 rounded-lg cursor-pointer"
+          onClick={onCloseModal}
+        >
+          X
+        </div>
+        {children}
+      </div>
     </dialog>
-  );
+  ) : null;
+
+  return dialog;
 }
